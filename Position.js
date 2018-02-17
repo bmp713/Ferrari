606d563800be6bc9ef1207deeb194e56ba9b3e97
Brandon Michael Piper <bmp713@yahoo.com>
2018-02-16 16:36:55 -0800

    Add files via upload

--- Position.js
+++ Position.js
@@ -5,23 +5,19 @@ var images = [];
 window.addEventListener("scroll", parallax);
 window.addEventListener("scroll", fixNavbar);
 
-images[0] = 'assets/images/Ferrari_3 (2).jpg';
-images[1] = 'assets/images/Ferrari_red.jpg';
-images[2] = 'assets/images/Ferrari_20.jpg';
-images[3] = 'assets/images/Ferrari_1.png';
-images[4] = 'assets/images/Ferrari_engine.png';
+images[0] = 'assets/images/Screenshot_0.png';
+images[1] = 'assets/images/Screenshot_1.png';
+images[2] = 'assets/images/Screenshot_2.png';
+images[3] = 'assets/images/Screenshot_3.png';
 
 function changeImage(){
 	var slide = document.getElementById("slide_image");
-	//var next = document.getElementById("next_image");
 
 	if( i < images.length - 1 ){
 		slide.src = images[i];
 		i++;
 	}
-	else{
-		i = 0;
-	}
+	else i = 0;
 }
 function openSlideShow(){
 	changeImage();
@@ -31,20 +27,22 @@ function openSlideShow(){
 function closeSlideShow(){
 	document.getElementById('slideshow').style.height = '0%';
 }
+
 function fixNavbar(){
 	if( window.pageYOffset >= 150 ){
 		document.getElementById("navbar").style.position = 'fixed';
-		document.getElementById('main').style.marginTop = '40px';
-		document.getElementById('navbar').style.maxWidth = '80%';
+		document.getElementById("navbar").style.width = '80%';
 		document.getElementById("navbar").style.top = '0';
+		document.getElementById('content').style.marginTop = '40px';
 	}
 	else{
 		document.getElementById("navbar").style.position = 'relative'
-		document.getElementById('main').style.marginTop = '0px';
-		document.getElementById('navbar').style.maxWidth = '100%';
+		document.getElementById("navbar").style.width = '100%';
 		document.getElementById("navbar").style.top = '100';
+		document.getElementById('content').style.marginTop = '0px';
 	}
 }
+
 function openSlideMenu(){
 	/* Viewport >m 400 pixels wide */
 	if ( window.matchMedia("(min-width: 768px)").matches ){
@@ -57,6 +55,7 @@ function openSlideMenu(){
 function closeSlideMenu(){
    	document.getElementById('slide-menu').style.width = '0'; 
 }
+
 function parallax(){
 	//document.getElementById("parallax_1").style.top = (window.pageYOffset/2) + 'px';	
 	document.getElementById("parallax_2").style.top = -(window.pageYOffset/2) + 'px';
@@ -69,8 +68,9 @@ function parallax(){
 		document.getElementById("social").style.opacity = '0';
 	}
 }
+
 function smoothScroll( elementId ){
-	var offset = 35; // Might need to make responsive
+	var offset = 50; // Might need to make responsive
 	var current = window.pageYOffset;
 	var destination = document.getElementById( elementId ).offsetTop;
 
@@ -93,73 +93,6 @@ function smoothScroll( elementId ){
 		}
 	}, 1 );
 }
-/*
-function changeImage(){
-
-	console.log("________________________________________________________");
-	if( i < (images.length - 1) ){
-		if( i == 0 ){
-			slide.src = images[i];
-			next.src = images[i + 1];
-			slide.style.zIndex = '10';
-			next.style.zIndex = '9';	
-		}
-		else{
-			console.log("1) i = " + i + ", slide-z = " + slide.style.zIndex + ", next-z = " + next.style.zIndex); 
-			if( slide.style.zIndex > next.style.zIndex){
-				console.log("11) i = " + i + ", slide-z = " + slide.style.zIndex + ", next-z = " + next.style.zIndex); 
-
-				slide.style.zIndex = "9";
-				next.style.zIndex = "10";	
-
-				slide.src = images[i + 1 ];
-				next.src = images[i];
-			}
-			else{
-				next.style.zIndex = "10";
-				slide.style.zIndex = "9";
-
-				next.src = images[i + 1];
-				slide.src = images[i];
-			}
-		}
-		i++;
-	}
-	else{
-		console.log("2) i = " + i + ", slide-z = " + slide.style.zIndex + ", next-z = " + next.style.zIndex); 
-		if( slide.style.zIndex > next.style.zIndex){	
-			console.log("22) i = " + i + ", slide-z = " + slide.style.zIndex + ", next-z = " + next.style.zIndex); 
-
-			slide.style.zIndex = "9";
-			next.style.zIndex = "10";	
-
-			slide.src = images[0 ];
-			next.src = images[i];
-		}
-		else{
-			slide.style.zIndex = "10";
-			next.style.zIndex = "9";	
-
-			slide.src = images[i];
-			next.src = images[0];
-		}
-		i = 0;
-	}
-}
-*/
-/*
-function zoomBackground(){
-
-   	var object = document.getElementById("body");
-
-	if( window.pageYOffset == window.innerHeight ){
-   		object.style.transform = "translate3d(" + x + "px," + y + "px, 0)";
-   	}
-   	else{
-
-   	}
-}
-*/
 
 
 
